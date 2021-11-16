faa00eb4c621511ed3beab5c68bc2a12df7f1eae
MoniBarrios <49417323+MoniBarrios@users.noreply.github.com>
2021-11-16 09:34:06 -0800

    Log In, Create Account, and Admin Landing Page

--- app/src/main/java/com/example/workoutplanner/AdminViewWorkouts.java
+++ app/src/main/java/com/example/workoutplanner/AdminViewWorkouts.java
@@ -2,6 +2,8 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class AdminViewWorkouts extends AppCompatActivity {
@@ -10,5 +12,10 @@ public class AdminViewWorkouts extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_view_workouts);
+    }
+
+    public static Intent intentFactory(Context ctx) {
+        Intent intent = new Intent(ctx, AdminViewWorkouts.class);
+        return intent;
     }
 }
